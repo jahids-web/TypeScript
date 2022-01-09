8483afd703e340cdae31e7096ee88ea252362897
Jahid Uddin <jahiduddin922@gmail.com>
2022-01-09 23:39:22 +0600

    TypeScript-Projects

--- TypeScript-Projects/src/script.js
+++ TypeScript-Projects/src/script.js
@@ -1,3 +1,4 @@
 var message = 'Hello World';
 console.log(message);
 console.log("Hello Ts");
+console.log("hello jahid");
--- TypeScript-Projects/src/script.ts
+++ TypeScript-Projects/src/script.ts
@@ -2,3 +2,5 @@ let message : string = 'Hello World';
 console.log(message);
 
 console.log("Hello Ts");
+
+console.log("hello jahid")
